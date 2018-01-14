c26a88394853a99f3294e8ba9291b0b6149acfa9
subpixel <borg.unimatrix0@gmail.com>
2018-01-14 21:52:57 +0000

    Initial commit baby! :)

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+        <title> Sweet login system by e-crop</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  
+  
+<header>
+      
+      <nav>
+          <div class="main-wrapper">
+      </nav>
+  
+  
+  
+  </header>  
+
+<section> </section>
+
+
+</body>
+</html>
