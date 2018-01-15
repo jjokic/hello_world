4d7b6eb07afae09015960b7405fa7a8913aa992a
subpixel <borg.unimatrix0@gmail.com>
2018-01-15 13:06:58 +0000

    update

--- index.php
+++ index.php
@@ -12,6 +12,21 @@
       
       <nav>
           <div class="main-wrapper">
+              
+              <ul>
+                  <li><a href="index.php">Home</a></li>
+                 
+              </ul>
+              <div class="nav-login">
+                  <form>
+                      <input type="text" name="uid" placeholder="Username / e-mail">
+                      <input type="password" name="pwd" placeholder="password">
+                      <button type="submit" name="submit">login</button>
+                  </form>
+                   <li><a href="signup.php">Signup</a></li>
+              </div>
+              
+          </div>
       </nav>
   
   
